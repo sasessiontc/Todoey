19fbcd905268f4435361dce81f58a156c86494df
Tim Morse <sasessiontc@gmail.com>
2018-12-29 16:44:10 -0500

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Tim Morse on 12/29/18.
+//  Copyright © 2018 Tim Morse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
