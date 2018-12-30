68562ab2b73dc12de841f411557b5a0d3ccfcc71
Tim Morse <sasessiontc@gmail.com>
2018-12-30 14:04:40 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Tim Morse on 12/30/18.
+//  Copyright © 2018 Tim Morse. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+    
+    
+}
